3ec9ff0675f667b8bf5766946a019b2297f2c194
JellyInLaw <61371616+JellyInLaw@users.noreply.github.com>
2020-08-17 01:48:10 +0500

    подключил шрифты и нормалайз.цсс

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
 import Post from "./Post";
-import "./styles/styles.css";
+import "./assets/styles/main.css";
 const post = new Post("webpack post title");
 console.log("Post to string: ", post.toString());
--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,7 @@
 const path = require("path");
 const HTMLWebpackPlugin = require("html-webpack-plugin");
 const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+const CopyPlugin = require("copy-webpack-plugin");
 
 module.exports = {
   context: path.resolve(__dirname, "src"),
@@ -15,11 +16,15 @@ module.exports = {
   plugins: [
     new HTMLWebpackPlugin({ template: "./index.html" }),
     new CleanWebpackPlugin(),
+    new CopyPlugin({
+      patterns: [{ from: "./assets/favicon.ico", to: "" }],
+    }),
   ],
   module: {
     rules: [
       { test: /\.css$/, use: ["style-loader", "css-loader"] },
       { test: /\.(png|jpeg|svg|gif)$/, use: ["file-loader"] },
+      { test: /\.(eot|woff|woff2|ttf)$/, use: ["file-loader"] },
     ],
   },
 };
