483c7795a1ba13bcdb1e65c620fd8b630bb79400
JellyInLaw <61371616+JellyInLaw@users.noreply.github.com>
2020-08-17 16:48:45 +0500

    + plugins
    cross-env
    optimize-css-assets-webpack-plugin
    terser-webpack-plugin

--- webpack.config.js
+++ webpack.config.js
@@ -3,9 +3,23 @@ const HTMLWebpackPlugin = require("html-webpack-plugin");
 const { CleanWebpackPlugin } = require("clean-webpack-plugin");
 const CopyPlugin = require("copy-webpack-plugin");
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const TerserPlugin = require("terser-webpack-plugin");
+const OptimizeCssAssetsPlugin = require("optimize-css-assets-webpack-plugin");
 
+//vars
 const isDev = process.env.NODE_ENV === "development";
+const isProd = !isDev;
 
+//function
+const optimization = () => {
+  const config = { splitChunks: { chunks: "all" } };
+  if (isProd) {
+    config.minimizer = [new TerserPlugin(), new OptimizeCssAssetsPlugin()];
+  }
+  return config;
+};
+
+//modules
 module.exports = {
   context: path.resolve(__dirname, "src"),
   entry: {
@@ -13,13 +27,16 @@ module.exports = {
     analytics: "./analytics.js",
   },
   output: {
-    filename: "js/[name].[contenthash].js",
+    filename: "js/[name].[hash].js",
     path: path.resolve(__dirname, "dist"),
   },
-  optimization: { splitChunks: { chunks: "all" } },
+  optimization: optimization(),
   devServer: { port: "8081", hot: isDev },
   plugins: [
-    new HTMLWebpackPlugin({ template: "./index.html" }),
+    new HTMLWebpackPlugin({
+      template: "./index.html",
+      minify: { collapseWhitespace: isProd },
+    }),
     new CleanWebpackPlugin(),
     new MiniCssExtractPlugin({ filename: "[name].[contenthash].css" }),
     new CopyPlugin({
