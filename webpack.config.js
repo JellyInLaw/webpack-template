8bb63c019f0ded7d076d06c681d10685dde636b9
JellyInLaw <61371616+JellyInLaw@users.noreply.github.com>
2020-08-17 10:12:22 +0500

    +miniCssExtractPlugin

--- webpack.config.js
+++ webpack.config.js
@@ -2,6 +2,9 @@ const path = require("path");
 const HTMLWebpackPlugin = require("html-webpack-plugin");
 const { CleanWebpackPlugin } = require("clean-webpack-plugin");
 const CopyPlugin = require("copy-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+
+const isDev = process.env.NODE_ENV === "development";
 
 module.exports = {
   context: path.resolve(__dirname, "src"),
@@ -13,16 +16,30 @@ module.exports = {
     filename: "js/[name].[contenthash].js",
     path: path.resolve(__dirname, "dist"),
   },
+  optimization: { splitChunks: { chunks: "all" } },
+  devServer: { port: "8081", hot: isDev },
   plugins: [
     new HTMLWebpackPlugin({ template: "./index.html" }),
     new CleanWebpackPlugin(),
+    new MiniCssExtractPlugin({ filename: "[name].[contenthash].css" }),
     new CopyPlugin({
       patterns: [{ from: "./assets/favicon.ico", to: "" }],
     }),
   ],
   module: {
     rules: [
-      { test: /\.css$/, use: ["style-loader", "css-loader"] },
+      {
+        test: /\.css$/,
+        use: [
+          {
+            loader: MiniCssExtractPlugin.loader,
+            options: {
+              hmr: isDev,
+            },
+          },
+          "css-loader",
+        ],
+      },
       {
         test: /\.(png|jpe?g|gif|svg)$/i,
         use: [
